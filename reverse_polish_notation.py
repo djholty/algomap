24d9e61e16c46c3b9f99cac3e3f95e0663235f97
djholty <87191635+djholty@users.noreply.github.com>
2024-10-27 12:11:19 -0400

    Create reverse_polish_notation.py

---
+++ reverse_polish_notation.py
@@ -0,0 +1,81 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stk = []
+        operator_map = {
+            '+' : operator.add,
+            '-' : operator.sub,
+            '*' : operator.mul,
+            '/' : operator.truediv,
+        }
+        if not tokens:
+            return None
+        for c in tokens:
+
+            try: #if this works, then c is an int
+                c = int(c)
+                stk.append(c)
+            except: #c is not an int, it's an operator so pop out last two ints, run the operator and append stack with new result
+                pop1 = stk.pop()
+                pop2nd = stk.pop()
+                if c == '/':  #need to truncate negative fractions up towards 0 and positive down with division, can't just use integer division operand or it won't truncate properly
+                    division = pop2nd/pop1
+                    if division > 0:
+                        newnum  = floor(division)
+                    else:
+                        newnum = ceil(division)
+                else:
+                    newnum = operator_map[c](pop2nd, pop1)
+                stk.append(newnum)
+            #print(stk)
+        if len(stk) == 1:
+            return stk[0]
+
+"""
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+
+    The valid operators are '+', '-', '*', and '/'.
+    Each operand may be an integer or another expression.
+    The division between two integers always truncates toward zero.
+    There will not be any division by zero.
+    The input represents a valid arithmetic expression in a reverse polish notation.
+    The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+
+Example 2:
+
+Input: tokens = ["4","13","5","/","+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+
+Example 3:
+
+Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+Output: 22
+Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+
+ 
+
+Constraints:
+
+    1 <= tokens.length <= 104
+    tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+
+
+"""

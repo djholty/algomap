3449865b5ac63089e16a817178fe8eb010d1b08f
djholty <87191635+djholty@users.noreply.github.com>
2024-12-13 13:21:19 -0500

    Update 1971_find_if_path_exists.py

--- 1971_find_if_path_exists.py
+++ 1971_find_if_path_exists.py
@@ -34,5 +34,69 @@ class Solution:
 
         return dfs(source)
 
+#STACK DFS
+from collections import defaultdict
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        if source == destination:
+            return True
+
+        graph = defaultdict(list)
+
+        #convert edges into adjacency list
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        seen =  set()
+
+        seen.add(source)
+        stack = [source]
+       
+
+        while stack:
+            node = stack.pop()
+            for neighbor in graph[node]:
+                if neighbor not in seen:
+                    if neighbor == destination:
+                        return True
+                    seen.add(neighbor)
+                    stack.append(neighbor)
+        
+        return False
+
+#QUEUE BFS
+from collections import defaultdict, deque
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        if source == destination:
+            return True
+
+        graph = defaultdict(list)
+
+        #convert edges into adjacency list
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        seen =  set()
+
+        seen.add(source)
+        q = deque()
+        q.append(source)
+       
+
+        while q:
+            node = q.popleft()
+            for neighbor in graph[node]:
+                if neighbor not in seen:
+                    if neighbor == destination:
+                        return True
+                    seen.add(neighbor)
+                    q.append(neighbor)
+        
+        return False
 
 

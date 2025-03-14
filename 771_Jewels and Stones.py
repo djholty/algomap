80682167890e7209aaffb989a453fd5d4978d7e7
djholty <87191635+djholty@users.noreply.github.com>
2025-03-14 08:24:23 -0400

    Update 771_Jewels and Stones.py

--- 771_Jewels and Stones.py
+++ 771_Jewels and Stones.py
@@ -1,1 +1,9 @@
 
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        jewelset = set([j for j in jewels])
+        c = 0
+        for s in stones:
+            if s in jewelset:
+                c += 1
+        return c
